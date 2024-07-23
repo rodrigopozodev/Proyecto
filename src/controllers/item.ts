23f36054349c367517ae3c5bd9afb7f3cbb24af4
rafapozo <rafapozo@hotmail.com>
2024-07-23 13:30:12 +0200

    error.handle.ts

--- src/controllers/item.ts
+++ src/controllers/item.ts
@@ -1,27 +1,39 @@
 import { Request, Response } from "express"
+import { handleHttp } from "../utils/error.handle";
 
 const getItem = (req:Request, res:Response) => {
     try{
     } catch (e) {
-        res.status(500);
-        res.send("ERROR_GET_ITEM");
+       handleHttp(res, 'ERROR_GET_ITEM') 
     }
 };
 
 const getItems = (req:Request, res:Response) => {
-
+    try{
+    } catch (e) {
+        handleHttp(res, 'ERROR_GET_ITEMS') 
+    }
 };
 
 const updateItem = (req:Request, res:Response) => {
-
+    try{
+    } catch (e) {
+        handleHttp(res, 'ERROR_UPDATE_ITEM') 
+    }
 };
 
 const postItem = (req:Request, res:Response) => {
-
+    try{
+    } catch (e) {
+        handleHttp(res, 'ERROR_POST_ITEM') 
+    }
 };
 
 const deleteItem = (req:Request, res:Response) => {
-
+    try{
+    } catch (e) {
+        handleHttp(res, 'ERROR_DELETE_ITEM') 
+    }
 };
 
 export {getItem, getItems, postItem, updateItem, deleteItem };
