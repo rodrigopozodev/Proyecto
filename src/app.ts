c120ed2216eb79b6f13fe9d25f3d76c484f1dd5f
rafapozo <rafapozo@hotmail.com>
2024-07-23 12:54:31 +0200

    segunda actualizacion con sqlite3

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,14 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import { router } from "./routes";
-import db from "./config/mongo"
+import { connectDB } from "./config/sqlite";
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
 app.use(router);
-db().then(() => console.log("Conexion Ready"))
-app.listen(PORT, () =>  console.log(`Listo por el puerto ${PORT}`))
+
+// Conectar a la base de datos
+connectDB().then(() => console.log("Conexión lista"));
+
+app.listen(PORT, () => console.log(`Servidor listo en el puerto ${PORT}`));
---
+++ src/config/sqlite.ts
@@ -0,0 +1,17 @@
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'database.sqlite'
+});
+
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+
+export { sequelize, connectDB };
